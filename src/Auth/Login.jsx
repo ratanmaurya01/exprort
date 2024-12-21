bb0ddbf360b3cf01db3e74b7c444940027f632d7
Ratan Maurya <151978027+ratanmaurya01@users.noreply.github.com>
2024-12-21 21:21:37 +0530

    Update the header component

---
+++ src/Auth/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+        <div>
+             <h6>Welcome to login page</h6>
+        </div>
+      
+    </div>
+  )
+}
